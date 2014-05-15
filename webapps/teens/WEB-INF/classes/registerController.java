6cd36a952d415f120808c09ec311529b678e0fc0
unknown <hosseina@wcs-it-m-00.wcs.weldon.k12.nc.us>
2014-05-15 16:07:03 -0400

    updates

--- webapps/teens/WEB-INF/classes/registerController.java
+++ webapps/teens/WEB-INF/classes/registerController.java
@@ -19,13 +19,26 @@ public class registerController extends HttpServlet
 	String firstName = request.getParameter("firstName");
 	String lastName = request.getParameter("lastName");
 	String grade = request.getParameter("grade");
+	String age = request.getParameter("age");
+	String school = request.getParameter("school");
+	String homeAddress = request.getParameter("homeAddress");
+	String homeCity = request.getParameter("homeCity");
+	String homeZIP = request.getParameter("homeZIP");
+	String cellularPhone = request.getParameter("cellularPhone");
+	String homePhone = request.getParameter("homePhone");
+	String studentEmail = request.getParameter("studentEmail");
+	String pgFirst = request.getParameter("pgFirst");
+	String pgLast = request.getParameter("pgLast");
+	String pgPhone = request.getParameter("pgPhone");
+	String pgEmail = request.getParameter("pgEmail");
+	String session1 = request.getParameter("session1");
+	String session2 = request.getParameter("session2");
 
-	createRegistration registration = new createRegistration(firstName, lastName, grade);
+	createRegistration registration = new createRegistration(firstName, lastName, grade, age, school, homeAddress, homeCity, homeZIP, cellularPhone, homePhone, studentEmail, pgFirst, pgLast, pgPhone, pgEmail, session1, session2);
 	session.setAttribute("registration", registration);
 	String address = "createRegistration.jsp";
 	RequestDispatcher rd = request.getRequestDispatcher(address);
 	rd.forward(request, response);
-			
 	
     }
 
